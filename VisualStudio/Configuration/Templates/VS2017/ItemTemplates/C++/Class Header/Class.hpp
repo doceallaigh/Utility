7d06683bf13981e02781b98c34cb23bb8729f2a7
doceallaigh <dylan.oceallaigh@gmail.com>
2017-07-12 18:39:25 -0400

    VS Templates
    Added file and project templates for Visual Studio

---
+++ VisualStudio/Configuration/Templates/VS2017/ItemTemplates/C++/Class Header/Class.hpp
@@ -0,0 +1,96 @@
+/* $safeitemname$.hpp
+ * Author: Dylan O'Ceallaigh
+ * $safeitemname$ description:
+ *     <$safeitemname$ description goes here>
+ * * */
+
+#pragma once
+
+// LIBRARY INCLUDES
+// LOCAL INCLUDES
+
+#ifndef CLASS_HPP
+class $safeitemname$ 
+{
+public:
+#pragma region Public Constructors & Destructor
+    // DEFAULT CONSTRUCTOR
+    // $safeitemname$ (void);
+
+    // COPY CONSTRUCTOR
+    // $safeitemname$ (const $safeitemname$ &original);
+
+    // MOVE CONSTRUCTOR
+    // $safeitemname$ (const $safeitemname$ &&original) noexcept;
+
+    // DESTRUCTOR
+    // ~$safeitemname$ (void) noexcept;
+#pragma endregion
+
+#pragma region Operators
+    // COPY ASSIGNEMENT OPERATOR
+    // $safeitemname$& operator= (const $safeitemname$ &original);
+
+    // MOVE ASSIGNEMENT OPERATOR
+    // $safeitemname$& operator= ($safeitemname$ &&original) noexcept;
+#pragma endregion
+
+#pragma region Public Virtual Methods
+    // PURE VIRTUAL METHODS
+    // VIRTUAL METHODS
+#pragma endregion
+
+#pragma region Public Non-virtual Methods
+    // NON-VOID METHODS
+    // VOID METHODS
+#pragma endregion
+
+#pragma region Public Fields
+    // SERVICES
+    // COLLECTIONS
+    // OBJECTS
+    // PRIMITIVES
+#pragma endregion
+
+protected:
+#pragma region Protected Virtual Methods
+    // PURE VIRTUAL METHODS
+    // VIRTUAL METHODS
+#pragma endregion
+
+#pragma region Protected Non-virtual Methods
+    // NON-VOID METHODS
+    // VOID METHODS
+#pragma endregion
+
+#pragma region Protected Fields
+    // SERVICES
+    // COLLECTIONS
+    // OBJECTS
+    // PRIMITIVES
+#pragma endregion
+
+private:
+#pragma region Private Constructors
+    // DEFAULT CONSTRUCTOR
+    // $safeitemname$ (void);
+#pragma endregion
+
+#pragma region Private Virtual Methods
+    // PURE VIRTUAL METHODS
+    // VIRTUAL METHODS
+#pragma endregion
+
+#pragma region Private Non-virtual Methods
+    // NON-VOID METHODS
+    // VOID METHODS
+#pragma endregion
+
+#pragma region Private Fields
+    // SERVICES
+    // COLLECTIONS
+    // OBJECTS
+    // PRIMITIVES
+#pragma endregion
+};
+#endif // !CLASS_HPP
---
+++ VisualStudio/Configuration/Templates/VS2017/ItemTemplates/C++/Class Implementation/Class.cpp
@@ -0,0 +1,58 @@
+#include "$safeitemname$.hpp"
+
+#pragma region Public Constructors & Destructor
+// DEFAULT CONSTRUCTOR
+// $safeitemname$::$safeitemname$ (void) { }
+
+// COPY CONSTRUCTOR
+// $safeitemname$::$safeitemname$ (const $safeitemname$ &original) { }
+
+// MOVE CONSTRUCTOR
+// $safeitemname$::$safeitemname$ (const $safeitemname$ &&original) noexcept { }
+
+// DESTRUCTOR
+// $safeitemname$::~$safeitemname$ (void) noexcept { }
+#pragma endregion
+
+#pragma region Operators
+// COPY ASSIGNEMENT OPERATOR
+// $safeitemname$& $safeitemname$::operator= (const $safeitemname$ &original) { }
+
+// MOVE ASSIGNEMENT OPERATOR
+// $safeitemname$& $safeitemname$::operator= ($safeitemname$ &&original) noexcept { }
+#pragma endregion
+
+#pragma region Public Virtual Methods
+// PURE VIRTUAL METHODS
+// VIRTUAL METHODS
+#pragma endregion
+
+#pragma region Public Non-virtual Methods
+// NON-VOID METHODS
+// VOID METHODS
+#pragma endregion
+
+#pragma region Protected Virtual Methods
+    // PURE VIRTUAL METHODS
+    // VIRTUAL METHODS
+#pragma endregion
+
+#pragma region Protected Non-virtual Methods
+    // NON-VOID METHODS
+    // VOID METHODS
+#pragma endregion
+
+#pragma region Private Constructors
+    // DEFAULT CONSTRUCTOR
+    // $safeitemname$ (void);
+#pragma endregion
+
+#pragma region Private Virtual Methods
+    // PURE VIRTUAL METHODS
+    // VIRTUAL METHODS
+#pragma endregion
+
+#pragma region Private Non-virtual Methods
+    // NON-VOID METHODS
+    // VOID METHODS
+#pragma endregion

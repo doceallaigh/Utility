1ee5d25cd28e434a1a920eb55f7b642ef7fc6547
doceallaigh <dylan.oceallaigh@gmail.com>
2017-07-12 19:44:06 -0400

    Added VS 2017 Interface template
    Added an interface item template for Visual Studio 2017 C++

---
+++ VisualStudio/Configuration/Templates/VS2017/ItemTemplates/C++/Interface Header/Interface.hpp
@@ -0,0 +1,24 @@
+/* $safeitemname$.hpp
+ * Interface description:
+ *     <$safeitemname$ description goes here>
+ * * */
+
+#pragma once
+
+// INCLUDES
+#pragma region Library Includes
+#pragma endregion
+
+#pragma region Local Includes
+#pragma endregion
+
+#ifndef $safeitemname$_hpp
+#define $safeitemname$_hpp
+class $safeitemname$ 
+{
+public:
+#pragma region Public Virtual Methods
+    // PURE VIRTUAL METHODS
+#pragma endregion
+};
+#endif // !$safeitemname$_hpp

a419790f28100cf1f17ca2f084d218efd44c9225
doceallaigh <dylan.oceallaigh@gmail.com>
2017-08-13 20:17:38 -0400

    Added Struct template
    Added a template file for basic POD objects

---
+++ VisualStudio/Configuration/VS2017/Templates/ItemTemplates/C++/Struct Header/Struct.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#pragma region Library Includes
+#pragma endregion
+
+#pragma region Local Includes
+#pragma endregion
+
+#pragma region Forward Declarations
+#pragma endregion
+
+#pragma region Type Definitions
+#pragma endregion
+
+/*! \brief <POD object description goes here>
+* */
+typedef struct $safeitemname$
+{
+public:
+} $safeitemname$;
